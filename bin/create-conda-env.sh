44287f900643b6d46841681c123538894fbdd9cf
Dmitry Lukovkin <dmitry.lukovkin@gmail.com>
2021-11-16 09:08:21 +0300

    * Modified to up-to-date verions and solvable env

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -2,6 +2,8 @@
 
 # create the conda environment
 export ENV_PREFIX=$PWD/env
-conda env create --prefix $ENV_PREFIX --file environment.yml --force
-conda activate $ENV_PREFIX
-source postBuild
+# conda init bash
+# conda env create --prefix $ENV_PREFIX --file environment.yml --force
+conda env create --name jupyterlab-ds  --file environment.yml --force -vv
+#conda activate jupyterlab-ds
+#source postBuild
